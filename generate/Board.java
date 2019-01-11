c7355b166623509f7d6e4be73986e64f38577302
Aviva Weinbaum <aw3156@barnard.edu>
2019-01-11 12:38:41 -0500

    added Board.java

--- generate/Board.java
+++ generate/Board.java
@@ -1,4 +1,20 @@
 public class Board {
 
+	private int[][] board;
 
+	public int[] fill() {
+		
+	}
+
+	public static checkHorizontal(int i) {
+
+	}
+
+	public static checkVertical(int i) {
+
+	}
+
+	public static checkBox() {
+
+	}
 }
