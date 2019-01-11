47c4ba0c8c14afeae0dcf6e28f6d0fe85057ee8c
Aviva Weinbaum <aw3156@barnard.edu>
2019-01-11 13:16:15 -0500

    edited Board.java

--- generate/Board.java
+++ generate/Board.java
@@ -3,26 +3,25 @@ public class Board {
 	private int[][] board;
 
 	public int[][] fill() {
-		while(column < 9){
-			while(row < 9{
+		while(column < 9) {
+			while(row < 9) {
 				//we will actually need two while loops, one to iterate through each row and one to iterate through all columns
 				int row = 0;
 				int column = 0;
 
-				int[row][column] = //random math int?
+				int[row][column] = (int)(Math.random()*9)+1; //random math int?
 
 				checkHorizontal(row);
-				checkvertical(column);
-				checkbox();
+				checkVertical(column);
+				checkBox(row, column);
 
 				row++;
-				column;
 			}
 		column++;
 		row = 0;
 	}
 
-		//return board
+		return board;
 
 	}
 
@@ -34,7 +33,25 @@ public class Board {
 
 	}
 
-	public static checkBox() {
+	public static checkBox(int i, int j) {
+		whichBox(i, j);
 
 	}
+
+	public int whichBox(int i, int j) {
+		int x = (i-(i%3))/3;
+		int y = (j-(j%3))/3;
+
+		if (x == 1 && y == 1) { return 1 }
+		else if (x == 1 && y == 2) { return 2 }
+		else if (x == 1 && y == 3) { return 3 }
+		else if  (x == 2 && y == 1) { return 4 }
+		else if  (x == 2 && y == 2) { return 5 }
+		else if  (x == 2 && y == 3) { return 6 }
+		else if  (x == 3 && y == 1) { return 7 }
+		else if  (x == 3 && y == 2) { return 8 }
+		else if  (x == 3 && y == 3) { return 9 }
+
+	}
+
 }
