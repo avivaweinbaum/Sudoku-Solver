13f684fff1ffd416042ef3717c4c2ab8eee2e5b7
Declangraymullen <dgraymullen@umass.edu>
2019-01-11 12:42:55 -0500

    Board changes

--- generate/Board.java
+++ generate/Board.java
@@ -2,8 +2,20 @@ public class Board {
 
 	private int[][] board;
 
-	public int[] fill() {
-		
+	public int[][] fill() {
+		while(notfull()){
+			int row = 0;
+			int column = 0;
+
+			int[row][column] = //random math int?
+
+			checkHorizontal(row);
+			checkvertical(column);
+			checkbox();
+		}
+
+		//return board
+
 	}
 
 	public static checkHorizontal(int i) {
